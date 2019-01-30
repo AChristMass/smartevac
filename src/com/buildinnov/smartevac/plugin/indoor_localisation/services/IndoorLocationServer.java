b9ed31f22882353bf0db2e7181a5cf142ceacfe1
Yazid <cb_medjdoub@esi.dz>
2019-01-30 13:31:53 +0100

    Initial commit

---
+++ src/com/buildinnov/smartevac/plugin/indoor_localisation/services/IndoorLocationServer.java
@@ -0,0 +1,16 @@
+package com.buildinnov.smartevac.plugin.indoor_localisation.services;
+
+
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * Entity that receive location requests from clients (Android, ...)
+ *
+ * */
+public class IndoorLocationServer {
+
+
+
+}
